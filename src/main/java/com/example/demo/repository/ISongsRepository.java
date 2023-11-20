2bef3c17a065e8aad0e88763ebcd4620c2c5323a
Cristian Martin Arias Saldaña <142331430+Lacanchaa@users.noreply.github.com>
2023-11-19 22:52:48 -0500

    ISongsRepository.java

--- src/main/java/com/example/demo/repository/ISongsRepository.java
+++ src/main/java/com/example/demo/repository/ISongsRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ISongsRepository extends JpaRepository<Songs,Integer> {
+   @Query("SELECT new Song(s.idSong, s.nameSong, s.gender, s.artist, s.fechaSong) FROM Song s")
+    List<Song> findAllSongs();
 }
