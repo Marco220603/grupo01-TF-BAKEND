330015fdfeccfe73c1c059ff13825fd00c51031d
HerbertPerez <u202118627@upc.edu.pe>
2023-11-19 22:36:13 -0500

    query
    query

--- src/main/java/com/example/demo/repository/ITemperRepository.java
+++ src/main/java/com/example/demo/repository/ITemperRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface ITemperRepository extends JpaRepository<Temper,Integer> {
     @Query(value = "SELECT id_temper, description_temper, min_score, name_temper, id_gender\n" +
-            "FROM temperaments\n" +
+            "FROM temper\n" +
             "ORDER BY min_score DESC\n" +
             "LIMIT 1;",nativeQuery = true)
     public List<String[]> TemperConMayorPuntaje();
